a26047b966ebfa93002a5eda0ae3ff0f9e421a07
Sytse Reitsma <sreitsma@moog.com>
2018-05-09 19:46:30 +0200

    wip

---
+++ README.md
@@ -0,0 +1,12 @@
+# ThrusterLights
+A small arduino program to add flame light effects to the rocket bed of my son.
+The hardware used for this is 3 NeoPixel Jewels and an AdaFruit Pro Trinket 5V/16MHz.
+
+### Uploading the firmware
+Select the Pro Trinket 5V/16MHz (FTDI) as the board.
+Select USBTiny as bootloader
+You may need to press the button on the board to enter bootloader mode.
+
+### NeoPixel Jewel connection
+The thrusters are each controller by their own pin (pin 4, 5, and 6 respectively).
+
--- ThrusterLights.ino
+++ ThrusterLights.ino
@@ -6,17 +6,47 @@
   #include <avr/power.h>
 #endif
 
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1
-#define PIN            6
+const int kThrusterAPin (4);
+const int kThrusterBPin (5);
+const int kThrusterCPin (6);
+const uint8_t kPixelsPerThruster (7);
 
-// How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      7
+struct Thruster {
+    Thruster (int inPin) :
+        mPixels (Adafruit_NeoPixel (kPixelsPerThruster, inPin, NEO_GRB + NEO_KHZ800))
+    {
+        mPixels.begin();
+    }
 
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+    void Update (uint16_t inIteration) {
+        const uint8_t kCenterPixel (0);
+
+        for(uint8_t pixel = 0; pixel < kPixelsPerThruster; ++pixel){
+            if (pixel == kCenterPixel) {
+                uint16_t intensity = (inIteration * 3) % 512;
+                if (intensity > 255) {
+                    intensity = 511 - intensity;
+                }
+
+                mPixels.setPixelColor(pixel, mPixels.Color(0, 0, intensity));
+            }
+            else {
+                const uint8_t divisor ((rand () % 10) + 1);
+                uint8_t red = 255 / divisor;
+                uint8_t green = 0x30 / divisor;
+                mPixels.setPixelColor(pixel, mPixels.Color(red, green, 0));
+            }
+
+            mPixels.show();
+        }
+    }
+
+    Adafruit_NeoPixel mPixels;
+};
+
+Thruster thrusterA (kThrusterAPin);
+Thruster thrusterB (kThrusterBPin);
+Thruster thrusterC (kThrusterCPin);
 
 int delayval = 500; // delay for half a second
 uint16_t iteration = 0;
@@ -27,31 +57,12 @@ void setup() {
   if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
 #endif
   // End of trinket special code
-
-  pixels.begin(); // This initializes the NeoPixel library.
 }
 
+
 void loop() {
-  const uint8_t kIntensity [6] = {0, 25, 51, 76, 102, 127};
-
-  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
-  for(int pixel = 0; pixel < NUMPIXELS; ++pixel){
-    if (pixel == 0) {
-        uint16_t intensity = (iteration * 2) % 512;
-        if (intensity > 255) {
-            intensity = 511 - intensity;
-        }
-
-        pixels.setPixelColor(pixel, pixels.Color(0, 0, intensity));
-    }
-    else {
-        int intensityIndex = ((pixel - 1) + iteration) % 6;
-        int intensity = kIntensity [intensityIndex];
-        pixels.setPixelColor(pixel, pixels.Color(intensity, 0, 0));
-    }
-    pixels.show(); // This sends the updated pixel color to the hardware.
-    delay(20);
-  }
-
+  thrusterA.Update (iteration);
+  thrusterB.Update (iteration);
+  thrusterC.Update (iteration);
   ++iteration;
 }
